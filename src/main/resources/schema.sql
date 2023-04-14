cabd8040a614df043ee235aed7b76337c4454f99
Noble Digital Services <morogoyo@gmail.com>
2023-04-14 08:11:08 -0400

    inital setup is completed and started to work on controller

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS api_lord.measurements (
+                         id int,
+                         comments varchar(255),
+                         subject varchar(255),
+                         length double,
+                         area double,
+                         count double
+);
